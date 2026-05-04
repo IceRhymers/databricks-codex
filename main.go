806b3bb869a9ddafa54c4c20e8146b02ea112329
Tanner <84605639+IceRhymers@users.noreply.github.com>
2026-05-04 14:31:51 -0500

    Drop token argument from ConstructGatewayURL call

--- main.go
+++ main.go
@@ -223,7 +223,7 @@ func main() {
 
 	gatewayURL := upstream
 	if gatewayURL == "" {
-		gatewayURL = ConstructGatewayURL(host, initialToken)
+		gatewayURL = ConstructGatewayURL(host)
 	}
 	log.Printf("databricks-codex: gateway URL: %s", gatewayURL)
 
